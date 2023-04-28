f3bd1ef044ba0ddd50e8e8fc0b2dabf66d98544d
BobTheSnob1 <102428955+BobTheSnob1@users.noreply.github.com>
2023-04-29 01:39:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const progressBar = document.getElementById('progressBar');
 const timer = document.getElementById('timer');
 
-const totalTime = 60; // Total time in seconds
+const startDate = new Date('2021-03-25T00:00:00');
+const endDate = new Date('2025-03-25T00:00:00');
+const totalTime = (endDate - startDate) / 1000;
 let currentTime = 0;
 
 function updateProgressBar() {
-  currentTime += 1;
+  const now = new Date();
+  currentTime = (now - startDate) / 1000;
   const percentage = (currentTime / totalTime) * 100;
   progressBar.style.width = `${percentage}%`;
 
-  const minutes = Math.floor(currentTime / 60);
-  const seconds = currentTime % 60;
-  timer.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+  const remainingTime = totalTime - currentTime;
+  const days = Math.floor(remainingTime / 86400);
+  const hours = Math.floor((remainingTime % 86400) / 3600);
+  const minutes = Math.floor((remainingTime % 3600) / 60);
+  const seconds = Math.floor(remainingTime % 60);
+  timer.textContent = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
   if (currentTime < totalTime) {
     setTimeout(updateProgressBar, 1000);
