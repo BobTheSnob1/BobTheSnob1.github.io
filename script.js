8db217392613467bb435293b71e31e21ef3b06ee
BobTheSnob1 <102428955+BobTheSnob1@users.noreply.github.com>
2023-04-29 04:18:40 +0200

    add year number

--- script.js
+++ script.js
@@ -11,32 +11,33 @@ let animationProgress = 0;
 updateCountdown();
 
 function updateProgressBar() {
-  const now = new Date();
-  currentTime = (now - startDate) / 1000;
-  const percentage = (currentTime / totalTime) * 100;
-
-  if (animationProgress < percentage) {
-    animationProgress += 1;
-    progressBar.style.width = `${animationProgress}%`;
-    percentageIndicator.textContent = `${Math.floor(animationProgress)}%`;
-    requestAnimationFrame(updateProgressBar);
-  } else {
-    progressBar.style.width = `${percentage}%`;
-    percentageIndicator.textContent = `${Math.floor(percentage)}%`;
-    setInterval(updateCountdown, 1000);
+    const now = new Date();
+    currentTime = (now - startDate) / 1000;
+    const percentage = (currentTime / totalTime) * 100;
+  
+    if (animationProgress < percentage) {
+      animationProgress += 0.5;
+      progressBar.style.width = `${animationProgress}%`;
+      percentageIndicator.textContent = `${Math.floor(animationProgress)}%`;
+      requestAnimationFrame(updateProgressBar);
+    } else {
+      progressBar.style.width = `${percentage}%`;
+      percentageIndicator.textContent = `${Math.floor(percentage)}%`;
+      setInterval(updateCountdown, 1000);
+    }
   }
-}
 
 function updateCountdown() {
-  const now = new Date();
-  currentTime = (now - startDate) / 1000;
-  const remainingTime = totalTime - currentTime;
-  const days = Math.floor(remainingTime / 86400);
-  const hours = Math.floor((remainingTime % 86400) / 3600);
-  const minutes = Math.floor((remainingTime % 3600) / 60);
-  const seconds = Math.floor(remainingTime % 60);
-  timer.textContent = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-}
+    const now = new Date();
+    currentTime = (now - startDate) / 1000;
+    const remainingTime = totalTime - currentTime;
+    const years = Math.floor(remainingTime / 31536000);
+    const days = Math.floor((remainingTime % 31536000) / 86400);
+    const hours = Math.floor((remainingTime % 86400) / 3600);
+    const minutes = Math.floor((remainingTime % 3600) / 60);
+    const seconds = Math.floor(remainingTime % 60);
+    timer.textContent = `${years}y ${days}d ${hours}h ${minutes}m ${seconds}s`;
+  }
 
 updateProgressBar();
 
